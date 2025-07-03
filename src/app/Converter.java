79985d1d3f3dce3877b742fa1d82ed2f7eea4fff
BuildTools <unconfigured@null.spigotmc.org>
2025-07-03 19:23:44 +0300

    Init commit

---
+++ src/app/Converter.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Converter {
+    private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+    }
+}
